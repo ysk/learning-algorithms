ea2d3fcfe1ced8cf332ffaf697b313c2fa2359cc
YusukeIkeda <yusuke_aono@yahoo.co.jp>
2024-04-01 21:58:42 +0900

    追加

---
+++ python/graph_1.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+data = [
+    [0, 1, 1, 0, 0],
+    [1, 0, 1, 1, 0],
+    [1, 1, 0, 0, 1],
+    [0, 1, 0, 0, 1],
+    [0, 0, 1, 1, 0]
+]
+
+node = ["(0)", "(1)", "(2)", "(3)", "(4)"]
+
+
+for y in range(5):
+    for x in range(y, 5):
+        if data[x][y] == 1 and data[x][y] == 1:
+            print(node[y] + "<->" + node[x])
+
---
+++ python/koch_curve.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+import tkinter
+import math
+
+x = 20
+y = 120
+l = 760
+a = 0
+
+def koch(n):
+    global x, y, l, a
+    if n > 0:
+        l /= 3
+        koch(n-1)
+        a += 60
+        koch(n-1)
+        a -= 120
+        koch(n-1)
+        a += 60
+        koch(n-1)
+        l *= 3
+    else:
+        nx = x + l*math.cos(math.radians(a))
+        ny = y + l*math.sin(math.radians(a))
+        cvs.create_line(x, y, nx, ny)
+        x = nx
+        y = ny
+
+root = tkinter.Tk()
+root.title("コッホ曲線")
+cvs = tkinter.Canvas(width=800, height=400, bg="white")
+cvs.pack()
+koch(5)
+root.mainloop()
+
